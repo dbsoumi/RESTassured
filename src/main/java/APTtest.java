90cf3aa0de9fcbe6631595f9829488fe2a3029ae
SoumiDBNCI <91730896+SoumiDBNCI@users.noreply.github.com>
2023-06-23 18:29:03 +0100

    Commit23/06/2023

---
+++ src/main/java/APTtest.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class APTtest {
+    @Test
+    void test1(){
+        Response response=RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println("status Code:"+ response.getStatusCode());
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("Push");
+    }
+}
